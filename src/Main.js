e1af0ce791ca51a039ef8d9512f8de5aaa8a44d5
Alexander Feinberg <68302328+alexanderfeinberg@users.noreply.github.com>
2022-10-05 09:55:51 -0400

    Initial commit

---
+++ src/Main.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+const Main = () => {
+  const [toggleOne, setToggleOne] = useState(false);
+  const [toggleTwo, setToggleTwo] = useState(false);
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log("UseEffect1 ran");
+  }, []);
+  useEffect(() => {
+    console.log("UseEffect2 ran");
+    if (toggleTwo) {
+      console.log("toggleTwo slice of state is true so this code runs");
+    }
+  }, [toggleTwo]);
+  useEffect(() => {
+    let myInterval = setInterval(() => {
+      console.log(`UseEffect3 with interval number ${count} is running`);
+    }, 1000);
+
+    return () => {
+      console.log(
+        `UseEffect3 cleanup ran.\nsetInterval number ${count} is being cleared out`
+      );
+      clearInterval(myInterval);
+    };
+  }, [count]);
+
+  return (
+    <div>
+      {console.log("rendered ir re-rendered")}
+      <h1>Main Componenet</h1>
+      <button onClick={() => setToggleOne(!toggleOne)}>ToggleOne</button>
+      <button onClick={() => setToggleTwo(!toggleTwo)}>ToggleTwo</button>
+      <button onClick={() => setCount(count + 1)}>{count}</button>
+    </div>
+  );
+};
+export default Main;
